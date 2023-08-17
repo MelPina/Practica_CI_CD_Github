3b40d92031d0eb1db36e7835234ccb390f484ec5
Mélida Mel Piña <101666856+MelPina@users.noreply.github.com>
2023-08-17 00:28:11 -0400

    Prueba unitaria

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Controllers;
+using Xunit;
+
+namespace WebApplication
+{
+    public class UnitTest1
+    {
+      
+            [Fact]
+            public void TestHelloWorld()
+            {
+                // Arrange
+                var controller = new HomeController();
+
+                // Act
+                var result = controller.Index() as ContentResult;
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.Equal("Hola Mundo desde C#!", result.Content);
+            }
+        
+    }
+}
